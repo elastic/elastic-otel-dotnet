c177833e7e3d0945031e9543e716cd12b1bfbffe
Martijn Laarman <Mpdreamz@gmail.com>
2024-02-28 15:39:24 +0100

    add simple logging assertions

--- tests/Elastic.OpenTelemetry.Tests/LoggingTests.cs
+++ tests/Elastic.OpenTelemetry.Tests/LoggingTests.cs
@@ -2,6 +2,7 @@
 // Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information
 
+using System.ComponentModel;
 using Elastic.OpenTelemetry.Diagnostics;
 using Elastic.OpenTelemetry.Diagnostics.Logging;
 using Microsoft.Extensions.Logging;
@@ -11,6 +12,9 @@ namespace Elastic.OpenTelemetry.Tests;
 
 public class TestLogger(ITestOutputHelper testOutputHelper) : ILogger
 {
+	private readonly List<string> _messages = new();
+	public IReadOnlyCollection<string> Messages => _messages;
+
 	public IDisposable BeginScope<TState>(TState state) where TState : notnull => NoopDisposable.Instance;
 
 	public bool IsEnabled(LogLevel logLevel)
@@ -19,14 +23,16 @@ public class TestLogger(ITestOutputHelper testOutputHelper) : ILogger
 	public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
 	{
 		var message = LogFormatter.Format(logLevel, eventId, state, exception, formatter);
+		_messages.Add(message);
 		testOutputHelper.WriteLine(message);
 		if (exception != null)
 			testOutputHelper.WriteLine(exception.ToString());
+
 	}
 
 	private class NoopDisposable : IDisposable
 	{
-		public static readonly NoopDisposable Instance = new NoopDisposable();
+		public static readonly NoopDisposable Instance = new();
 		public void Dispose()
 		{ }
 	}
@@ -55,5 +61,14 @@ public class LoggingTests(ITestOutputHelper output)
             activity?.SetStatus(ActivityStatusCode.Ok);
         }
 
-    }
+		//assert preamble information gets logged
+		logger.Messages.Should().ContainMatch("*Elastic OpenTelemetry Distribution:*");
+
+		// assert agent initialized confirmation and stack trace gets dumped.
+		logger.Messages.Should().ContainMatch("*AgentBuilder initialized*");
+
+		// very lenient format check
+		logger.Messages.Should().ContainMatch("[*][*][*][Info]*");
+
+	}
 }
