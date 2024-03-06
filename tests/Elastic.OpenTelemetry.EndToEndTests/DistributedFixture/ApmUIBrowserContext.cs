043501a86e105d341ca926ec0957df0848d3cce4
Martijn Laarman <Mpdreamz@gmail.com>
2024-03-06 13:45:11 +0100

    artifact trace output

--- tests/Elastic.OpenTelemetry.EndToEndTests/DistributedFixture/ApmUIBrowserContext.cs
+++ tests/Elastic.OpenTelemetry.EndToEndTests/DistributedFixture/ApmUIBrowserContext.cs
@@ -29,7 +29,8 @@ public class ApmUIBrowserContext : IAsyncLifetime
 	public IBrowser Browser { get; private set; } = null!;
 	public IPlaywright HeadlessTester { get; private set; } = null!;
 
-	private static string _bootstrapTraceName = "test_bootstrap";
+	private const string BootstrapTraceName = "test_bootstrap";
+
 	public async Task InitializeAsync()
 	{
 		var username = _configuration["E2E:BrowserEmail"]?.Trim() ?? string.Empty;
@@ -37,7 +38,7 @@ public class ApmUIBrowserContext : IAsyncLifetime
 		Program.Main(["install", "chromium"]);
 		HeadlessTester = await Playwright.CreateAsync();
 		Browser = await HeadlessTester.Chromium.LaunchAsync();
-		var page = await OpenApmLandingPage(_bootstrapTraceName);
+		var page = await OpenApmLandingPage(BootstrapTraceName);
 		try
 		{
 			await page.GetByRole(AriaRole.Textbox, new() { Name = "email" }).FillAsync(username);
@@ -52,7 +53,7 @@ public class ApmUIBrowserContext : IAsyncLifetime
 		}
 		catch (Exception e)
 		{
-			await StopTrace(page, _bootstrapTraceName);
+			await StopTrace(page, BootstrapTraceName);
 			Console.WriteLine(e);
 			throw;
 		}
@@ -123,7 +124,7 @@ public class ApmUIBrowserContext : IAsyncLifetime
 		if (string.IsNullOrWhiteSpace(testName))
 			await page.Context.Tracing.StopAsync(new());
 		//never dump bootstrap failures on CI since these might leak credentials
-		else if (testName == _bootstrapTraceName && !string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("CI")))
+		else if (testName == BootstrapTraceName && !string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("CI")))
 			await page.Context.Tracing.StopAsync(new());
 		else
 		{
