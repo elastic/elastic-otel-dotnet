d2c3b9f405a8196d60aa53fb72c08496d93fe3fb
Steve Gordon <sgordon@hotmail.co.uk>
2024-03-19 16:06:32 +0000

    Add initial documentation

---
+++ examples/Example.MinimalApi/Program.cs
@@ -0,0 +1,52 @@
+using System.Diagnostics;
+using Example.Api;
+using OpenTelemetry;
+using OpenTelemetry.Trace;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+	.AddHttpClient() // Adds IHttpClientFactory
+	.AddOpenTelemetry() // Adds the OpenTelemetry SDK
+		.WithTracing(t => {
+			t.AddAspNetCoreInstrumentation(); // Configure tracing to instrument ASP.NET Core
+			t.AddSource(Api.ActivitySourceName);
+		});
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.MapGet("/", (IHttpClientFactory httpClientFactory) => Api.HandleRoot(httpClientFactory));
+
+app.Run();
+
+namespace Example.Api
+{
+	internal static class Api
+	{
+		public static string ActivitySourceName = "CustomActivitySource";
+		private static readonly ActivitySource ActivitySource = new(ActivitySourceName);
+
+		public static async Task<IResult> HandleRoot(IHttpClientFactory httpClientFactory)
+		{
+			using var client = httpClientFactory.CreateClient();
+
+			using var activity = ActivitySource.StartActivity("DoingStuff", ActivityKind.Internal);
+			activity?.SetTag("custom-tag", "TagValue");
+
+			await Task.Delay(100);
+			var response = await client.GetAsync("http://elastic.co"); // using this URL will require 2 redirects
+			await Task.Delay(50);
+
+			if (response.StatusCode == System.Net.HttpStatusCode.OK)
+			{
+				activity?.SetStatus(ActivityStatusCode.Ok);
+				return Results.Ok();
+			}
+
+			activity?.SetStatus(ActivityStatusCode.Error);
+			return Results.StatusCode(500);
+		}
+	}
+}
---
+++ src/Elastic.OpenTelemetry/AgentBuilderOptions.cs
@@ -0,0 +1,45 @@
+﻿// Licensed to Elasticsearch B.V under one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry.Trace;
+
+namespace Elastic.OpenTelemetry;
+
+/// <summary>
+/// Expert options to provide to <see cref="AgentBuilder"/> to control its initial OpenTelemetry registration
+/// </summary>
+public record AgentBuilderOptions
+{
+	/// <summary>
+	/// Provide an additional logger to the internal file logger.
+	/// <para>
+	/// The agent will always log to file if a Path is provided using the <c>ELASTIC_OTEL_LOG_DIRECTORY</c>
+	/// environment variable.</para>
+	/// </summary>
+	public ILogger? Logger { get; init; }
+
+	/// <summary>
+	/// Provides an <see cref="IServiceCollection"/> to register the agent into.
+	/// If null a new local instance will be used.
+	/// </summary>
+	public IServiceCollection? Services { get; init; }
+
+	/// <summary>
+	/// The initial activity sources to listen to.
+	/// <para>>These can always later be amended with <see cref="TracerProviderBuilder.AddSource"/></para>
+	/// </summary>
+	public string[] ActivitySources { get; init; } = [];
+
+	/// <summary>
+	/// Stops <see cref="AgentBuilder"/> to register OLTP exporters, useful for testing scenarios
+	/// </summary>
+	public bool SkipOtlpExporter { get; init; }
+
+	/// <summary>
+	/// Optional name which is used when retrieving OTLP options.
+	/// </summary>
+	public string? OtlpExporterName { get; init; }
+}
