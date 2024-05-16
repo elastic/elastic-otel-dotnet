823152844b02a0b3845ae62a64875803aa1427e5
Steve Gordon <sgordon@hotmail.co.uk>
2024-05-16 15:09:08 +0100

    Enhance resource attributes for better interop with Elastic APM
    Sets `service.instance.id`, `host.id` and `host.name`.

--- src/Elastic.OpenTelemetry/Extensions/ResourceBuilderExtensions.cs
+++ src/Elastic.OpenTelemetry/Extensions/ResourceBuilderExtensions.cs
@@ -1,14 +1,82 @@
 // Licensed to Elasticsearch B.V under one or more agreements.
 // Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information
-using Elastic.OpenTelemetry.SemanticConventions;
 
+using System.Diagnostics;
+using Elastic.OpenTelemetry.SemanticConventions;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using OpenTelemetry.ResourceDetectors.Host;
 using OpenTelemetry.Resources;
 
 namespace Elastic.OpenTelemetry.Extensions;
 
-internal static class ResourceBuilderExtensions
+/// <summary>
+/// Extension methods for <see cref="ResourceBuilder"/>.
+/// </summary>
+public static class ResourceBuilderExtensions
 {
+	private static readonly string InstanceId = Guid.NewGuid().ToString();
+
+	/// <summary>
+	/// For advanced scenarios, where the all Elastic defaults are not disabled (essentially using the "vanilla" OpenTelemetry SDK),
+	/// this method can be used to add Elastic resource defaults to the <see cref="ResourceBuilder"/>.
+	/// </summary>
+	/// <remarks>
+	/// After clearing the <see cref="ResourceBuilder"/> the following are added in order:
+	/// <list type="bullet">
+	/// <item>A default, fallback <c>service.name</c>.</item>
+	/// <item>A default, unqiue <c>service.instance.id</c>.</item>
+	/// <item>The telemetry SDK attributes via <c>AddTelemetrySdk()</c>.</item>
+	/// <item>The Elastic telemetry distro attributes.</item>
+	/// <item>Adds resource attributes parsed from OTEL_RESOURCE_ATTRIBUTES, OTEL_SERVICE_NAME environment variables
+	/// via <c>AddEnvironmentVariableDetector()</c>.</item>
+	/// <item>Host attributes, <c>host.name</c> and <c>host.id</c> (on supported targets).</item>
+	/// </list>
+	/// <para>These mostly mirror what the vanilla SDK does, but allow us to ensure that certain resources attributes that the
+	/// Elastic APM backend requires to drive the UIs are present in some form. Any of these may be overridden by further
+	/// resource configuration.</para>
+	/// </remarks>
+	/// <param name="builder">A <see cref="ResourceBuilder"/> that will be configured with Elastic defaults.</param>
+	/// <returns>The <see cref="ResourceBuilder"/> for chaining calls.</returns>
+	public static ResourceBuilder AddElasticResourceDefaults(this ResourceBuilder builder) =>
+		builder.AddElasticResourceDefaults(NullLogger.Instance);
+
+	internal static ResourceBuilder AddElasticResourceDefaults(this ResourceBuilder builder, ILogger logger)
+	{
+		var defaultServiceName = "unknown_service";
+
+		try
+		{
+			var processName = Process.GetCurrentProcess().ProcessName;
+			if (!string.IsNullOrWhiteSpace(processName))
+			{
+				defaultServiceName = $"{defaultServiceName}:{processName}";
+			}
+		}
+		catch
+		{
+			// GetCurrentProcess can throw PlatformNotSupportedException
+		}
+
+		builder
+			.Clear()
+			.AddAttributes(new Dictionary<string, object>
+			{
+				{ ResourceSemanticConventions.AttributeServiceName, defaultServiceName },
+				{ ResourceSemanticConventions.AttributeServiceInstanceId, InstanceId }
+			})
+			.AddTelemetrySdk()
+			.AddDistroAttributes()
+			.AddEnvironmentVariableDetector();
+
+#if NET462_OR_GREATER || NET6_0_OR_GREATER
+		builder.AddDetector(new HostDetector(logger));
+#endif
+
+		return builder;
+	}
+
 	internal static ResourceBuilder AddDistroAttributes(this ResourceBuilder builder) =>
 		builder.AddAttributes(new Dictionary<string, object>
 		{
