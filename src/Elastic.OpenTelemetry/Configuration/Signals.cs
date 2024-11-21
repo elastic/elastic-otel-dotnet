c7ac3b872b44467f23e81333e54a0bf9e958cc07
Steve Gordon <sgordon@hotmail.co.uk>
2024-11-21 19:06:44 +0000

    Non code change to trigger CI

--- src/Elastic.OpenTelemetry/Configuration/Signals.cs
+++ src/Elastic.OpenTelemetry/Configuration/Signals.cs
@@ -11,18 +11,18 @@ namespace Elastic.OpenTelemetry.Configuration;
 [EnumExtensions]
 public enum Signals
 {
-	/// <summary> No Elastic defaults will be included, acting effectively as a vanilla OpenTelemetry </summary>
+	/// <summary> No Elastic defaults will be included, acting effectively as a vanilla OpenTelemetry.</summary>
 	None,
 
-	/// <summary> Include Elastic Distribution of OpenTelemetry .NET tracing defaults</summary>
+	/// <summary> Include Elastic Distribution of OpenTelemetry .NET tracing defaults.</summary>
 	Traces = 1 << 0, //1
 
-	/// <summary> Include Elastic Distribution of OpenTelemetry .NET metrics defaults</summary>
+	/// <summary> Include Elastic Distribution of OpenTelemetry .NET metrics defaults.</summary>
 	Metrics = 1 << 1, //2
 
-	/// <summary> Include Elastic Distribution of OpenTelemetry .NET logging defaults</summary>
+	/// <summary> Include Elastic Distribution of OpenTelemetry .NET logging defaults.</summary>
 	Logs = 1 << 2, //4
 
-	/// <summary> (default) Include all Elastic Distribution of OpenTelemetry .NET logging defaults</summary>
+	/// <summary> (default) Include all Elastic Distribution of OpenTelemetry .NET logging defaults.</summary>
 	All = ~0
 }
