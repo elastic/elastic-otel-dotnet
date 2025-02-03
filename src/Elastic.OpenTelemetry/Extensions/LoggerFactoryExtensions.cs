646db5c868ccf68d1e21cbd8d4217e0815448a6d
Steve Gordon <sgordon@hotmail.co.uk>
2025-02-03 16:09:28 +0000

    Add ILoggerFactory extensions

---
+++ src/Elastic.OpenTelemetry/Extensions/LoggerFactoryExtensions.cs
@@ -0,0 +1,14 @@
+﻿// Licensed to Elasticsearch B.V under one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information
+
+using Elastic.OpenTelemetry.Diagnostics;
+using Microsoft.Extensions.Logging;
+
+namespace Elastic.OpenTelemetry;
+
+internal static class LoggerFactoryExtensions
+{
+	public static ILogger CreateElasticLogger(this ILoggerFactory loggerFactory) =>
+		loggerFactory.CreateLogger(CompositeLogger.LogCategory);
+}
